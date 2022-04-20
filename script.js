e31bbfec4db100bdcd45df9ef5df59b372ac8a30
Felipe <fe.donatto@outlook.com>
2022-04-20 16:25:24 -0300

    requisito 6 feito

---
+++ script.js
@@ -0,0 +1,7 @@
+window.onload = function(event){
+    const onloadColor = document.querySelectorAll('.color')[0]
+    onloadColor.classList.add('selected')
+    // onloadColor.addEventListener('mouseover', function(evento){
+    //     console.log('oi')
+    // })
+}
