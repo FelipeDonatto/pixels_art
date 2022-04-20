6e05baca25e03d11c0c1d4cd38bdb552679f3d33
Felipe <fe.donatto@outlook.com>
2022-04-20 18:35:54 -0300

    requisito 8 feito

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
-window.onload = function(event){
-    const onloadColor = document.querySelectorAll('.color')[0]
-    onloadColor.classList.add('selected')
-    // onloadColor.addEventListener('mouseover', function(evento){
-    //     console.log('oi')
-    // })
+const colorPalette = document.getElementById('color-palette')
+const pixels = document.getElementById('pixel-board')
+
+
+function changeSelected(event){
+    const color = document.querySelector('.selected')
+    color.classList.remove('selected')
+    event.target.classList.add('selected');
 }
+  
+colorPalette.addEventListener('click', changeSelected);
+// similar ao do exercicio do course
+
+pixels.addEventListener('click', function(event){
+    const colortochange = document.querySelector('.selected')
+    let newid = colortochange.id
+    if(event.target.id !== 'pixel-board'){
+    event.target.id = newid
+    }
+    // demorei umas duas horas nessa mas acredito que consegui fazer da maneira mais eficiente
+    // feeling kinda great ngl
+})
